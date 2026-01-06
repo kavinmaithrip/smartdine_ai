80dc42528ae31196ff29019fd4b2fd88809383b3
kavinmaithrip <kavinmaithri@gmail.com>
2026-01-06 23:03:33 +0530

    Delete frontend/src/components/QueryInput.jsx

--- frontend/src/components/QueryInput.jsx
+++
@@ -1,9 +0,0 @@
-export default function QueryInput({ query, setQuery }) {
-  return (
-    <input
-      placeholder="Feeling tired? Craving something spicy?"
-      value={query}
-      onChange={e => setQuery(e.target.value)}
-    />
-  );
-}
